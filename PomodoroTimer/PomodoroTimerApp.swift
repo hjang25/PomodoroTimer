284e93d988b4b98f0420ab959635bc837d2278b1
Hajin Jang <hajinjang25@gmail.com>
2024-10-05 12:22:17 -0400

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Hajin Jang on 2024/10/05.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
